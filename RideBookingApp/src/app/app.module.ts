3aabb6fea3e86f66928b1b122150203673fb69f1
noellamwanzia <noellamwanzia@gmail.com>
2022-06-05 16:40:08 +0300

    📦import mat card module.

--- RideBookingApp/src/app/app.module.ts
+++ RideBookingApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { MorningTripComponent } from './home/schedules/morning-trip/morning-trip.component';
 import { EveningTripComponent } from './home/schedules/evening-trip/evening-trip.component';
@@ -23,7 +24,8 @@ import { EveningTripComponent } from './home/schedules/evening-trip/evening-trip
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
