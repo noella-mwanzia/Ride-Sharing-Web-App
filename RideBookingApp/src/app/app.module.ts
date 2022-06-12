9cb10c7a482fd494b09b4cc4b124d376a679d2a1
noellamwanzia <noellamwanzia@gmail.com>
2022-06-12 17:54:43 +0300

    📦import needed modules

--- RideBookingApp/src/app/app.module.ts
+++ RideBookingApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,11 +12,17 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
 
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { MorningTripComponent } from './home/schedules/morning-trip/morning-trip.component';
 import { EveningTripComponent } from './home/schedules/evening-trip/evening-trip.component';
 import { BookRideFormComponent } from './forms/book-ride-form/book-ride-form.component';
+import { BookRideDialogComponent } from './dialogs/book-ride-dialog/book-ride-dialog.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +31,8 @@ import { BookRideFormComponent } from './forms/book-ride-form/book-ride-form.com
     HomePageComponent,
     MorningTripComponent,
     EveningTripComponent,
-    BookRideFormComponent
+    BookRideFormComponent,
+    BookRideDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +42,14 @@ import { BookRideFormComponent } from './forms/book-ride-form/book-ride-form.com
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
