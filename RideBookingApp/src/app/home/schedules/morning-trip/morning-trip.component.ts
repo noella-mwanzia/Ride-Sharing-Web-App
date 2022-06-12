0f49d0bca007b5e2072151b951e4597c14737ea3
noellamwanzia <noellamwanzia@gmail.com>
2022-06-12 17:59:17 +0300

    ✨add logic to open dialog when booking button is pressed.

--- RideBookingApp/src/app/home/schedules/morning-trip/morning-trip.component.ts
+++ RideBookingApp/src/app/home/schedules/morning-trip/morning-trip.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { BookRideDialogComponent } from 'src/app/dialogs/book-ride-dialog/book-ride-dialog.component';
 
 @Component({
   selector: 'app-morning-trip',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MorningTripComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+   
+  bookRide(){
+    const dialogRef = this.dialog.open(BookRideDialogComponent, {
+      width: '350px',
+      height: '400px',
+      data: { },
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+  }
 }
