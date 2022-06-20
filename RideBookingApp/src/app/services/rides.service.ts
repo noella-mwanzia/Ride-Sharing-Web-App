32ba34fd2f5df82b821bfc23a38cb6e6440b9b00
noellamwanzia <noellamwanzia@gmail.com>
2022-06-20 23:26:41 +0300

    ✨add logic to add route schedules

--- RideBookingApp/src/app/services/rides.service.ts
+++ RideBookingApp/src/app/services/rides.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+import { RideSchedules } from '../interfaces/ride-schedules';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RidesService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  //add designated bus stops in a particular area
+  addAreaStage(route:RideSchedules, period: 'morning' | 'evening'){
+    const collectionRef = this.db.collection<RideSchedules>(`rides/imara-daima/route-schedules/${period}`);
+    return collectionRef.add(route);
+  }
 }
