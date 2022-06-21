dc4bde96094ce2a2b70d45b0696ed4c16d8d9e2f
noellamwanzia <noellamwanzia@gmail.com>
2022-06-21 08:32:27 +0300

    ✨Add logic for user sign up with email and password.

--- RideBookingApp/src/app/services/auth-service.service.ts
+++ RideBookingApp/src/app/services/auth-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Roles } from '../interfaces/roles';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  signUpWithEmailAndPassword(displayName: string, email: string, password: string, roles: Roles)
+{
+  return  this.afAuth
+              .createUserWithEmailAndPassword(email,password)
+              .then(res => console.log(res.user))
+              .catch(error => console.log(error))
+}
 }
