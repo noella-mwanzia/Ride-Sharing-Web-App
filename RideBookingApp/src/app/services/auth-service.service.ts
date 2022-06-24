1bcadb480ad75992546fd871a439e0a8a8d4af0c
noellamwanzia <noellamwanzia@gmail.com>
2022-06-25 01:01:12 +0300

    ✨add method to add user details to user collection after they sign up

--- RideBookingApp/src/app/services/auth-service.service.ts
+++ RideBookingApp/src/app/services/auth-service.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/compat/auth";
-import { Roles } from '../interfaces/roles';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+import { Roles, UserType } from '../interfaces/roles';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth,
+              private db: AngularFirestore) { }
 
 
-signUpWithEmailAndPassword(displayName: string, email: string, password: string, roles: Roles)
+signUpWithEmailAndPassword(form: any , userType: UserType)
 {
   return  this.afAuth
-              .createUserWithEmailAndPassword(email,password)
-              .then(res => console.log(res.user))
+              .createUserWithEmailAndPassword(form.email,form.password)
+              .then(res => this.addToUsersCollection(res.user.displayName, res.user.email, res.user.uid ,userType))
               .catch(error => console.log(error))
 }
+
+private addToUsersCollection(displayName: string, email: string, userId: string, userType: UserType)
+{
+  const userDetails = { 
+    displayName: displayName,
+    email: email,
+    uId: userId,
+    role: userType
+  }
+
+  this.db.collection(`users`).doc(userId).set(userDetails)
+      .then((res) => console.log(`User successfully added to users collection`))
+      .catch(error => console.log(error))
+
 }
+}
+
