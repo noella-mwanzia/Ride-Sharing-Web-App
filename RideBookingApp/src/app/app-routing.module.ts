06e739bf14542a491fbf7a17c0621276afe0d0f3
noellamwanzia <noellamwanzia@gmail.com>
2022-06-25 01:02:39 +0300

    ✨add routing

--- RideBookingApp/src/app/app-routing.module.ts
+++ RideBookingApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
