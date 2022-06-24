e45357b4801da9dc4b85b34cba2f4d019802f4f1
noellamwanzia <noellamwanzia@gmail.com>
2022-06-25 01:02:20 +0300

    ✨use service to sign up new user on component level

--- RideBookingApp/src/app/auth/sign-up/sign-up.component.ts
+++ RideBookingApp/src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { Roles, UserType } from 'src/app/interfaces/roles';
+
+
 
 @Component({
   selector: 'app-sign-up',
@@ -10,7 +14,8 @@ export class SignUpComponent implements OnInit {
 
   signUpForm!: FormGroup;
 
-  constructor( private fb: FormBuilder) { }
+  constructor(  private fb: FormBuilder,
+                private authService: AuthService) { }
 
   ngOnInit(): void {
     this.initForm()
@@ -20,9 +25,13 @@ export class SignUpComponent implements OnInit {
     this.signUpForm = this.fb.group({
       "name": ['',Validators.required],
       "phoneNumber": ['', Validators.required],
-      "email": ['', Validators.required],
+      "email": ['', [Validators.required,Validators.email]],
       "password": ['', Validators.required]
     })
+  }
+
+  signUpUser(){
+    this.authService.signUpWithEmailAndPassword(this.signUpForm.value, UserType.captain)
   }
 
 }
