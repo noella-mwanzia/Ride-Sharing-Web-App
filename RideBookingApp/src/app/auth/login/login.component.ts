8a9a6d8f0a782ebd4a9641aece3e8350398e7b76
noellamwanzia <noellamwanzia@gmail.com>
2022-06-25 18:18:12 +0300

    🔧invoke login service method from component.

--- RideBookingApp/src/app/auth/login/login.component.ts
+++ RideBookingApp/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +11,27 @@ export class LoginComponent implements OnInit {
 
   logInForm: FormGroup;
 
-  constructor( private _fb: FormBuilder) { }
+  constructor(  private _fb: FormBuilder,
+                private _authService: AuthService) { }
 
   ngOnInit(): void {
     this.initForm()
   }
 
-  initForm(){
+  initForm()
+  {
     this.logInForm = this._fb.group({
       "email": ['',[Validators.required]],
       "password": ['', Validators.required]
     })
+  }
+
+  login()
+  {
+    const email = this.logInForm.controls['email'].value;
+    const password = this.logInForm.controls['password'].value;
+
+    this._authService.loginWithEmailAndPassword(email,password);
   }
 
 }
