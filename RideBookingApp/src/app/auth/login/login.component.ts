e9436c417975da29bbbd767e0ae1ae9db0c2ee2c
noellamwanzia <noellamwanzia@gmail.com>
2022-06-25 15:47:47 +0300

    ✨make login form reactive.

--- RideBookingApp/src/app/auth/login/login.component.ts
+++ RideBookingApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logInForm: FormGroup;
+
+  constructor( private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm(){
+    this.logInForm = this._fb.group({
+      "email": ['',[Validators.required]],
+      "password": ['', Validators.required]
+    })
   }
 
 }
