7de68e2f201cb126f43d995689003c21044ba397
noellamwanzia <noellamwanzia@gmail.com>
2022-07-02 22:27:02 +0300

    ✨add logic to retrieve the active user's details.

--- RideBookingApp/src/app/services/active-user.service.ts
+++ RideBookingApp/src/app/services/active-user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, of, switchMap, tap } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 
 /**
@@ -11,10 +14,24 @@ import { AngularFireAuth } from "@angular/fire/compat/auth";
 })
 export class ActiveUserService {
 
-  constructor( private afAuth: AngularFireAuth) { }
+  constructor(  private _afAuth: AngularFireAuth,
+                private _afs: AngularFirestore) { }
 
   //check current authentication state of user.
-  public getActiveUser(){
-    return this.afAuth.authState;
+  public getActiveUser()
+  {
+    return this._afAuth
+                .user
+                .pipe(switchMap(user =>
+                          // Switch to subscription, if doc changes everything changes.
+                          ((user && user.uid) ? this._afs.doc<User>(`users/${user.uid}`).valueChanges()
+                                              : of(null)) as Observable<User>),
+                      tap(u => {
+                        if(!!u){
+                          console.log(`[UserService] Retrieved user ${(u as User).id}`)
+                        }
+                        else{
+                          console.log(`User not set`);
+                        }}));
   }
 }
